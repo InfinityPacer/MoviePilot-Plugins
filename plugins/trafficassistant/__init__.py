7a950d5f6e70196b3d0b76288aa99b769ecdeba8
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-06-03 19:52:10 +0800

    fix version

--- plugins/trafficassistant/__init__.py
+++ plugins/trafficassistant/__init__.py
@@ -32,7 +32,7 @@ class TrafficAssistant(_PluginBase):
     # 插件图标
     plugin_icon = "https://github.com/InfinityPacer/MoviePilot-Plugins/raw/main/icons/trafficassistant.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
