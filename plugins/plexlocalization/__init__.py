159aed30548d3a6be23c4d98a623055301e7487a
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-06-15 00:06:32 +0800

    feat: PlexLocalization v1.5

--- plugins/plexlocalization/__init__.py
+++ plugins/plexlocalization/__init__.py
@@ -36,7 +36,7 @@ class PlexLocalization(_PluginBase):
     # 插件图标
     plugin_icon = "https://github.com/InfinityPacer/MoviePilot-Plugins/raw/main/icons/plexlocalization.png"
     # 插件版本
-    plugin_version = "1.4"
+    plugin_version = "1.5"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
