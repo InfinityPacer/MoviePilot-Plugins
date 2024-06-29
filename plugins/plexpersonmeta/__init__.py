1af338dd76324026b21e595a94947647c109baa3
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-06-29 22:36:44 +0800

    feat: PlexPersonMeta v1.3

--- plugins/plexpersonmeta/__init__.py
+++ plugins/plexpersonmeta/__init__.py
@@ -40,7 +40,7 @@ class PlexPersonMeta(_PluginBase):
     # 插件图标
     plugin_icon = "https://github.com/InfinityPacer/MoviePilot-Plugins/raw/main/icons/plexpersonmeta.png"
     # 插件版本
-    plugin_version = "1.2"
+    plugin_version = "1.3"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
