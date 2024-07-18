a501b1f0049526f625b538ac4da15fa89b80d0f4
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-07-18 22:06:45 +0800

    add mi hosts plugin

---
+++ plugins/mihosts/__init__.py
@@ -0,0 +1,310 @@
+import threading
+from datetime import datetime, timedelta
+from typing import Any, List, Dict, Tuple, Optional
+
+import pytz
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from requests import Response
+
+from app.core.config import settings
+from app.log import logger
+from app.plugins import _PluginBase
+from app.schemas.types import NotificationType
+from app.utils.http import RequestUtils
+
+lock = threading.Lock()
+
+
+class MIHosts(_PluginBase):
+    # 插件名称
+    plugin_name = "米家Hosts"
+    # 插件描述
+    plugin_desc = "定时将本地Hosts同步至米家Hosts。"
+    # 插件图标
+    plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/mihosts.png"
+    # 插件版本
+    plugin_version = "1.0"
+    # 插件作者
+    plugin_author = "InfinityPacer"
+    # 作者主页
+    author_url = "https://github.com/InfinityPacer"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "mihosts_"
+    # 加载顺序
+    plugin_order = 36
+    # 可使用的用户级别
+    auth_level = 1
+
+    # region 私有属性
+
+    # 是否开启
+    _enabled = False
+    # 立即运行一次
+    _onlyonce = False
+    # 任务执行间隔
+    _cron = None
+    # 发送通知
+    _notify = False
+    # 定时器
+    _scheduler = None
+    # 退出事件
+    _event = threading.Event()
+
+    # endregion
+
+    def init_plugin(self, config: dict = None):
+        if not config:
+            return
+
+        self._enabled = config.get("enabled")
+        self._onlyonce = config.get("onlyonce")
+        self._cron = config.get("cron")
+        self._notify = config.get("notify")
+
+        # 停止现有任务
+        self.stop_service()
+
+        # 启动服务
+        self._scheduler = BackgroundScheduler(timezone=settings.TZ)
+        if self._onlyonce:
+            logger.info(f"{self.plugin_name}服务，立即运行一次")
+            self._scheduler.add_job(
+                func=self.action,
+                trigger="date",
+                run_date=datetime.now(tz=pytz.timezone(settings.TZ)) + timedelta(seconds=3),
+                name=f"{self.plugin_name}",
+            )
+            # 关闭一次性开关
+            self._onlyonce = False
+            config["onlyonce"] = False
+            self.update_config(config=config)
+
+        # 启动服务
+        if self._scheduler.get_jobs():
+            self._scheduler.print_jobs()
+            self._scheduler.start()
+
+    def get_state(self) -> bool:
+        return self._enabled
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        pass
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        pass
+
+    def get_service(self) -> List[Dict[str, Any]]:
+        """
+        注册插件公共服务
+        [{
+            "id": "服务ID",
+            "name": "服务名称",
+            "trigger": "触发器：cron/interval/date/CronTrigger.from_crontab()",
+            "func": self.xxx,
+            "kwargs": {} # 定时器参数
+        }]
+        """
+        if self._enabled and self._cron:
+            logger.info(f"{self.plugin_name}定时服务启动，时间间隔 {self._cron} ")
+            return [{
+                "id": MIHosts.__name__,
+                "name": f"{self.plugin_name}服务",
+                "trigger": CronTrigger.from_crontab(self._cron),
+                "func": self.action,
+                "kwargs": {}
+            }]
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        try:
+            if self._scheduler:
+                self._scheduler.remove_all_jobs()
+                if self._scheduler.running:
+                    self._event.set()
+                    self._scheduler.shutdown()
+                    self._event.clear()
+                self._scheduler = None
+        except Exception as e:
+            logger.info(str(e))
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        """
+        拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
+        """
+        return [
+            {
+                'component': 'VForm',
+                'content': [
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'enabled',
+                                            'label': '启用插件',
+                                            'hint': '开启后插件将处于激活状态',
+                                            'persistent-hint': True,
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'notify',
+                                            'label': '发送通知',
+                                            'hint': '是否在特定事件发生时发送通知',
+                                            'persistent-hint': True,
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'onlyonce',
+                                            'label': '立即运行一次',
+                                            'hint': '插件将立即运行一次',
+                                            'persistent-hint': True,
+                                        },
+                                    }
+                                ],
+                            }
+                        ],
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'cron',
+                                            'label': '运行周期',
+                                            'placeholder': '5位cron表达式',
+                                            'hint': '使用cron表达式指定执行周期，如 0 8 * * *',
+                                            'persistent-hint': True,
+                                        },
+                                    }
+                                ],
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '注意：可配合自定义Hosts以及Cloudflare IP优选插件，实现米家路由Cloudflare优选'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '注意：可以通过访问米家 -> 路由 -> 自定义Hosts，点击右上角复制链接，'
+                                                    '从而获取到对应的 access_token、device_id 等数据'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ], {
+            "enabled": False,
+            "notify": True,
+            "cron": "0 6 * * *"
+        }
+
+    def get_page(self) -> List[dict]:
+        pass
+
+    def action(self):
+        """
+        action
+        """
+        pass
+
+    @staticmethod
+    def make_request(method: str, app_id: str, device_id: str, client_id: str,
+                     token: str, hosts: str, scope: str = "1+1000+3") -> Optional[Response]:
+        base_url = "https://www.gorouter.info/api-third-party/service/internal/custom_host_"
+        url = f"{base_url}{'get' if method.lower() == 'get' else 'set'}"
+
+        data = {
+            "appId": app_id,
+            "deviceId": device_id,
+            "clientId": client_id,
+            "scope": scope,
+            "token": token,
+            "hosts": hosts
+        }
+
+        request = RequestUtils(ua=settings.USER_AGENT, referer="https://s.miwifi.com")
+        response = request.request(method=method, url=url, data=data, raise_exception=True)
+        return response
+
+    def __send_message(self, title: str, text: str):
+        """
+        发送消息
+        """
+        if not self._notify:
+            return
+
+        self.post_message(mtype=NotificationType.SiteMessage, title=title, text=text)
