b9436caa7a68853fd1400bd769c8d17e7d9c5e6e
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-07-23 21:57:18 +0800

    feat: 支持远程命令切换自动跳过

--- plugins/plexautoskip/__init__.py
+++ plugins/plexautoskip/__init__.py
@@ -2,12 +2,14 @@ import threading
 from pathlib import Path
 from typing import Any, List, Dict, Tuple
 
+from app.core.event import eventmanager, Event
 from app.log import logger
 from app.plugins import _PluginBase
 from app.plugins.plexautoskip.resources.log import getLogger
 from app.plugins.plexautoskip.resources.server import getPlexServer
 from app.plugins.plexautoskip.resources.settings import Settings
 from app.plugins.plexautoskip.resources.skipper import Skipper
+from app.schemas.types import EventType
 
 
 class PlexAutoSkip(_PluginBase):
@@ -40,6 +42,8 @@ class PlexAutoSkip(_PluginBase):
 
     # 是否开启
     _enabled = False
+    # 自动跳过
+    _auto_skip = None
     # 退出事件
     _event = threading.Event()
 
@@ -50,18 +54,19 @@ class PlexAutoSkip(_PluginBase):
 
     def init_plugin(self, config: dict = None):
         if not config:
-            return False
+            return
 
         self.stop_service()
 
         self._enabled = config.get("enabled")
-        if self._enabled:
+        self._auto_skip = config.get("auto_skip")
+        if self._enabled and self._auto_skip:
             self._skipper_config = config.get("skipper_config", self.default_ini.read_text(encoding="utf-8"))
             self.user_ini.write_text(self._skipper_config, encoding="utf-8")
-            self._start_auto_skip()
+            self.__start_auto_skip()
 
     @staticmethod
-    def _get_logger(plugin_name: str):
+    def __get_logger(plugin_name: str):
         """
         获取模块的logger
         """
@@ -74,7 +79,7 @@ class PlexAutoSkip(_PluginBase):
                     return _logger
         return getLogger(__name__)
 
-    def _start_auto_skip(self):
+    def __start_auto_skip(self):
         logger.info("已开启PlexAutoSkip，正在准备停止历史服务")
 
         self.stop_service()
@@ -82,10 +87,11 @@ class PlexAutoSkip(_PluginBase):
         logger.info("正在初始化相关服务")
 
         try:
-            log = self._get_logger(self.plugin_name.lower())
+            log = self.__get_logger(self.plugin_name.lower())
             skip_settings = Settings(logger=log)
             self.update_config({
                 "enabled": self._enabled,
+                "auto_skip": self._auto_skip,
                 "skipper_config": self.user_ini.read_text(encoding="utf-8")
             })
 
@@ -109,7 +115,15 @@ class PlexAutoSkip(_PluginBase):
         定义远程控制命令
         :return: 命令关键字、事件、描述、附带数据
         """
-        pass
+        return [{
+            "cmd": "/switch_plex_skip",
+            "event": EventType.PluginAction,
+            "desc": "切换PlexAutoSkip",
+            "category": "Plex",
+            "data": {
+                "action": "switch_plex_auto_skip"
+            }
+        }]
 
     def get_api(self) -> List[Dict[str, Any]]:
         pass
@@ -129,7 +143,7 @@ class PlexAutoSkip(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
-                                    'md': 6
+                                    'md': 4
                                 },
                                 'content': [
                                     {
@@ -145,7 +159,23 @@ class PlexAutoSkip(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
-                                    'md': 6
+                                    'md': 4
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'auto_skip',
+                                            'label': '自动跳过',
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4
                                 },
                                 'content': [
                                     {
@@ -314,6 +344,7 @@ class PlexAutoSkip(_PluginBase):
             }
         ], {
             "enabled": False,
+            "auto_skip": True,
             "skipper_config": self.default_ini.read_text(encoding="utf-8")
         }
 
@@ -349,3 +380,32 @@ class PlexAutoSkip(_PluginBase):
             self._event.clear()  # 立即重置事件状态
         except Exception as e:
             logger.error(f"Error stopping PlexAutoSkip service: {e}", exc_info=True)  # 记录堆栈信息
+
+    @eventmanager.register(EventType.PluginAction)
+    def switch_auto_skip(self, event: Event = None):
+        """
+        切换Plex自动跳过
+        """
+        if not event:
+            return
+        event_data = event.event_data
+        if not event_data or event_data.get("action") != "switch_plex_auto_skip":
+            return
+
+        if self._enabled:
+            logger.info("接收到外部命令，正在切换自动跳过")
+            self._auto_skip = not self._auto_skip
+            if self._auto_skip:
+                self.__start_auto_skip()
+            else:
+                self.stop_service()
+
+            config = self.get_config()
+            config["auto_skip"] = self._auto_skip
+            self.update_config(config=config)
+
+            status = "开启" if self._auto_skip else "关闭"
+            self.post_message(channel=event.event_data.get("channel"),
+                              title=f"【PlexAutoSkip】",
+                              text=f"自动跳过已{status}",
+                              userid=event.event_data.get("user"))
