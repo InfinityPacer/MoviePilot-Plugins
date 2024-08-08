99819a720b2d27f0318d74d1256a6675e9c726e7
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-08-08 17:29:26 +0800

    fix HitAndRun v1.2

--- plugins/hitandrun/__init__.py
+++ plugins/hitandrun/__init__.py
@@ -37,7 +37,7 @@ class HitAndRun(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/hitandrun.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
