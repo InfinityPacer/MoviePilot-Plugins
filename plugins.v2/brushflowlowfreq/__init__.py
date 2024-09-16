6d7d747d58c5bf23064078ccd3983403da832d35
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-09-16 12:21:10 +0800

    feat(BrushFlowLowFreq): Update version

--- plugins.v2/brushflowlowfreq/__init__.py
+++ plugins.v2/brushflowlowfreq/__init__.py
@@ -258,7 +258,7 @@ class BrushFlowLowFreq(_PluginBase):
     # 插件图标
     plugin_icon = "brush.jpg"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "3.9"
     # 插件作者
     plugin_author = "jxxghp,InfinityPacer"
     # 作者主页
