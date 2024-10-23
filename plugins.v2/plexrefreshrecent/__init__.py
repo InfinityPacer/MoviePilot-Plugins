be8ff113837a4474f9cb1a873513ad4011482a81
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-10-23 12:51:10 +0800

    feat(PlexRefreshRecent): v1.5

--- plugins.v2/plexrefreshrecent/__init__.py
+++ plugins.v2/plexrefreshrecent/__init__.py
@@ -25,7 +25,7 @@ class PlexRefreshRecent(_PluginBase):
     # 插件图标
     plugin_icon = "Plex_A.png"
     # 插件版本
-    plugin_version = "1.4"
+    plugin_version = "1.5"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
