6182b516fa1a65c0bb7310b2b13637e165c1a17c
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-10-26 05:09:27 +0800

    feat(Commands): adjust plugin order

--- plugins.v2/commands/__init__.py
+++ plugins.v2/commands/__init__.py
@@ -29,7 +29,7 @@ class Commands(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "commands_"
     # 加载顺序
-    plugin_order = 31
+    plugin_order = 29
     # 可使用的用户级别
     auth_level = 1
 
