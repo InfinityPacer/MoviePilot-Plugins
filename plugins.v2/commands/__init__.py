66185e6b9e0e6cb58d66a23c12cf666c5245ed09
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-11-23 13:34:50 +0800

    update plugin order

--- plugins.v2/commands/__init__.py
+++ plugins.v2/commands/__init__.py
@@ -29,7 +29,7 @@ class Commands(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "commands_"
     # 加载顺序
-    plugin_order = 29
+    plugin_order = 42
     # 可使用的用户级别
     auth_level = 1
 
