f87006203fdcef88ae8a8719e898220bcc30254e
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-11-27 00:47:21 +0800

    feat(SmartRename): adapt for v2.1.1 compatibility

--- plugins.v2/smartrename/__init__.py
+++ plugins.v2/smartrename/__init__.py
@@ -20,7 +20,7 @@ class SmartRename(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/smartrename.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
