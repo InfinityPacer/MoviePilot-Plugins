d0df415a0a51319473b1ff7ceb3b6e1dac644037
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-11-29 00:02:36 +0800

    feat(ServiceManager): v1.1

--- plugins.v2/servicemanager/__init__.py
+++ plugins.v2/servicemanager/__init__.py
@@ -22,7 +22,7 @@ class ServiceManager(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/servicemanager.png"
     # 插件版本
-    plugin_version = "1.0.1"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
