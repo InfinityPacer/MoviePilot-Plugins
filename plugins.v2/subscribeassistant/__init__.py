b19a8cfcf17103fc9bb88afd271aa49592b6a47d
InfinityPacer <160988576+InfinityPacer@users.noreply.github.com>
2024-12-19 16:02:22 +0800

    feat(SubscribeAssistant): v1.1

--- plugins.v2/subscribeassistant/__init__.py
+++ plugins.v2/subscribeassistant/__init__.py
@@ -38,7 +38,7 @@ class SubscribeAssistant(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/subscribeassistant.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
