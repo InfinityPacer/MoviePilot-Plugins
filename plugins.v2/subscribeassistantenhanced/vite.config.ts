d5ab75e057365678291cd1348ded9661b77ba89b
InfinityPacer <InfinityPacer@gmail.com>
2026-06-14 03:45:35 +0800

    fix(subscribeassistantenhanced): normalize vue config build output

--- plugins.v2/subscribeassistantenhanced/vite.config.ts
+++ plugins.v2/subscribeassistantenhanced/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   ],
   build: {
     target: 'esnext',
-    minify: false,
+    minify: 'esbuild',
     cssCodeSplit: true,
     rollupOptions: {
       input: './src/Config.vue',
