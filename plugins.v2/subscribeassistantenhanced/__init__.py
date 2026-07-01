3509aa537de0fddeea8425dc45008956c950e922
InfinityPacer <InfinityPacer@gmail.com>
2026-07-01 14:41:55 +0800

    chore(subscribeassistantenhanced): release 0.4.9

--- plugins.v2/subscribeassistantenhanced/__init__.py
+++ plugins.v2/subscribeassistantenhanced/__init__.py
@@ -85,7 +85,7 @@ class SubscribeAssistantEnhanced(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/subscribeassistantenhanced.png"
     # 插件版本
-    plugin_version = "0.4.8"
+    plugin_version = "0.4.9"
     # 插件作者
     plugin_author = "InfinityPacer"
     # 作者主页
