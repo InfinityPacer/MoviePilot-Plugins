b47314a3efe082e3953b6cbe9cc15f2241564bd5
InfinityPacer <InfinityPacer@gmail.com>
2026-08-21 09:43:58 +0800

    fix(v3): refresh SubscribeAssistantEnhanced frontend bundle

--- plugins.v3/subscribeassistantenhanced/frontend/dist/assets/remoteEntry.js
+++ plugins.v3/subscribeassistantenhanced/frontend/dist/assets/remoteEntry.js
@@ -3,7 +3,7 @@ const currentImports = {};
       let moduleMap = {
 "./Config":()=>{
       dynamicLoadingCss(["__federation_expose_Config-COZtkKfx.css"], false, './Config');
-      return __federation_import('./__federation_expose_Config-CaQEeIYR.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      return __federation_import('./__federation_expose_Config-D69E8ckT.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
