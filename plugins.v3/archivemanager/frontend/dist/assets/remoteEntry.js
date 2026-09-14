4b3180f8b8f9dd5044830b629a6c7f2243db3ee9
InfinityPacer <InfinityPacer@gmail.com>
2026-09-14 10:07:54 +0800

    fix(archivemanager): pin frontend build tool

--- plugins.v3/archivemanager/frontend/dist/assets/remoteEntry.js
+++ plugins.v3/archivemanager/frontend/dist/assets/remoteEntry.js
@@ -3,7 +3,7 @@ const currentImports = {};
       let moduleMap = {
 "./Config":()=>{
       dynamicLoadingCss(["__federation_expose_Config-BkLSqEPN.css"], false, './Config');
-      return __federation_import('./__federation_expose_Config-CjAPdbPq.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      return __federation_import('./__federation_expose_Config-CPBKPLX6.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
